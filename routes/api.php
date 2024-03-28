9c0b34c905a3ff73cef49c3734cd6636c9464948
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-03-28 00:47:20 -0300

    feat/Implemento Sanctum para CRSF y Autentificacion, y Bounce para Roles y Permisos

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-Route::post('api/v1/tasks', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers','middleware' => ['auth:sanctum']], function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('comments', CommentController::class);
+    Route::apiResource('files', FileController::class);
+});
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,60 @@
 <?php
 
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\FileController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'api/v1', 'namespace'=>'App\Http\Controllers'], function(){
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('tasks', TaskController::class);
-    Route::apiResource('comments', CommentController::class);
-    Route::apiResource('files', FileController::class);
+Route::post('/login', function (Request $request) {
+    // ... tu lógica de autenticación (validar email y password) ...
 
+    $user = User::where('email', $request->email)->first();
+
+    if ($user && Hash::check($request->password, $user->password)) {
+
+        $token = $user->createToken('auth_token');
+
+        $response = [
+            'success' => true,
+            'token' => $token->plainTextToken,
+            'user' => $user, // Opcional: incluye información del usuario
+        ];
+        return response()->json($response, 200);
+        
+    } else {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
 });
+
+// Route::group(['prefix'=>'api/v1', 'namespace'=>'App\Http\Controllers'], function(){
+//     Route::apiResource('users', UserController::class);
+//     Route::apiResource('tasks', TaskController::class);
+//     Route::apiResource('comments', CommentController::class);
+//     Route::apiResource('files', FileController::class);
+
+// });
+
+// Route::get('/setup', function(){
+//     $credentials=[
+//         'email'=>'admin@admin.com',
+//         'password'=> 'password'
+//     ];
+//     if(!Auth::attempt($credentials)){
+//         $user =  new \App\Models\User();
+//         $user->name  = 'Admin';
+//         $user->role  = 0;
+//         $user->email  = $credentials['email'];
+//         $user->password  = Hash::make($credentials['password']);
+//         $user->save();
+//     }
+//     if(Auth::attempt($credentials)){
+//         $user=Auth::user();
+//         $adminToken = $user->createToken('');
+//         $token = $user->createToken('auth_token')->plainTextToken;
+//     }
+     
+// });
